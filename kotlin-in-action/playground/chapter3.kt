6f1069abdaebf263d5b5329bc340abb6c326edb4
doyoung0205 <doyoung0205@naver.com>
2022-07-31 00:29:40 +0900

    정적인 유틸리티 클래스 없애기: 최상위 함수와 프로퍼티

--- kotlin-in-action/playground/chapter3.kt
+++ kotlin-in-action/playground/chapter3.kt
@@ -1,3 +1,5 @@
+import `kotlin-in-action`.playground.joinToString
+
 fun main() {
     joinToStringSample()
 
@@ -8,24 +10,6 @@ fun collectionGenerate() {
     val set = hashSetOf(1, 3, 5)
     val list = arrayListOf(1, 7, 22)
     val map = hashMapOf(1 to "one", 7 to "seven")
-}
-
-
-fun <T> joinToString(
-    collection: Collection<T>,
-    separator: String = " ",
-    prefix: String = "[",
-    postfix: String = "]"
-): String {
-    val result = StringBuilder(prefix)
-
-    for ((index, element) in collection.withIndex()) {
-        if (index > 0) result.append(separator)
-        result.append(element)
-    }
-
-    result.append(postfix)
-    return result.toString()
 }
 
 fun joinToStringSample() {
